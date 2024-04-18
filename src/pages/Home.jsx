ad9205cea89d63ec269303a22862d94b274d8295
5Cord <dima2v@bk.ru>
2024-04-18 23:19:27 +0300

    Full frontend project on mern

---
+++ src/pages/Home.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Grid from '@mui/material/Grid';
+import { useState } from 'react';
+
+import axios from '../Axios';
+
+import { Post } from '../components/Post';
+import { TagsBlock } from '../components/TagsBlock';
+import { CommentsBlock } from '../components/CommentsBlock';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
+
+export const Home = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector(state => state.auth.data);
+  const { posts, tags } = useSelector(state => state.posts);
+  const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
+
+  React.useEffect(() => {
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
+  }, [])
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const sortedPosts = [...posts.items].sort((a, b) => b.viewsCount - a.viewsCount);
+
+  return (
+    <>
+      <Tabs style={{ marginBottom: 15 }} value={value} onChange={handleChange} aria-label="basic tabs example">
+        <Tab label="Новые" />
+        <Tab label="Популярные" />
+      </Tabs>
+      <Grid container spacing={4}>
+        <Grid xs={8} item>
+          {(isPostsLoading ? [...Array(5)] : (!value ? posts.items : sortedPosts)).map((obj, index) => isPostsLoading ? <Post key={index} isLoading={true} /> : (
+            <Post
+              id={obj._id}
+              title={obj.title}
+              imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
+              user={obj.user}
+              createdAt={obj.createdAt}
+              viewsCount={obj.viewsCount}
+              commentsCount={3}
+              tags={obj.tags}
+              isEditable={userData?._id === obj.user._id}
+            />
+          ))}
+        </Grid>
+        <Grid xs={4} item>
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+          <CommentsBlock
+            items={[
+              {
+                user: {
+                  fullName: 'Вася Пупкин',
+                  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                },
+                text: 'Это тестовый комментарий',
+              },
+              {
+                user: {
+                  fullName: 'Иван Иванов',
+                  avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                },
+                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+              },
+            ]}
+            isLoading={false}
+          />
+        </Grid>
+      </Grid>
+
+    </>
+  );
+};
